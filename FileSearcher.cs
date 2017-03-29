116ad8b32ba03d04e392356eb395f0a7b35e9c22
CamIAm <rcmontgo@gmail.com>
2017-03-28 23:33:17 -0400

    Queue added to add individual file and folder names to list

--- FileSearcher.cs
+++ FileSearcher.cs
@@ -1,10 +1,13 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace ConsoleApplication
 {
     public class FileSearcher
     {
+        internal Stack<int> SlashPlacementStack{ get;set; }
+        internal Queue<int> SlashPlacementQueue{ get;set; }
         internal string CurrentDirectory{ get; set; }
         public event EventHandler<FileFoundArgs> FileFound;
         internal event EventHandler<SearchDirectoryArgs> DirectoryChanged
@@ -13,6 +16,11 @@ namespace ConsoleApplication
                 remove { directoryChanged -= value; }
             }
         private EventHandler<SearchDirectoryArgs> directoryChanged;
+        public FileSearcher()
+        {
+             SlashPlacementQueue = SlashPlacementQueue ?? new Queue<int>();
+             SlashPlacementStack = SlashPlacementStack ?? new Stack<int>();
+        }
         public void SearchDirectories(string directory, string searchPattern, bool searchSubDirs = true)
         {
             if (searchSubDirs)
@@ -23,8 +31,9 @@ namespace ConsoleApplication
                 foreach (var dir in allDirectories)
                 {
                     //DirectoryInfo(directory,dir);
-                    int numOfSlashes = numOfSlashesInPath(directory, dir);
-                    System.Console.WriteLine($"Total numOfSlashes: {numOfSlashes}");
+                    //int numOfSlashes = NumOfSlashesInPath(directory, dir);
+                    //Stack<int> stackPlacement = BuildDirectoryTree()
+                    //System.Console.WriteLine($"Total numOfSlashes: {numOfSlashes}");
                     directoryChanged?.Invoke(this,
                                 new SearchDirectoryArgs(dir, totalDirs, completedDirs++));
                     // Recursively search this child directory:
@@ -45,21 +54,71 @@ namespace ConsoleApplication
         {
             foreach (var file in Directory.EnumerateFiles(directory, searchPattern))
             {
-                //DirectoryInfo(CurrentDirectory,file);
-                int numOfSlashes = numOfSlashesInPath(CurrentDirectory, file);
+                char[] charArray = GetDirectoryCharArray(CurrentDirectory, file);
+                var fileToArrayComparison = file.Substring(CurrentDirectory.LastIndexOf('/'));
+                if(charArray.Length != fileToArrayComparison.Length){
+                    System.Console.WriteLine("charArray does not include of of file");// use comparer to determine which variable doesnt include which
+                }
+                Queue<int> queueSlashPlacement = BuildDirectoryTreeViaQueue(charArray);
+                GetCharArrayBySlicesQueue(charArray, queueSlashPlacement);
+                while(queueSlashPlacement.Count>0)
+                {
+                    int stackIndex = queueSlashPlacement.Dequeue();
+                    System.Console.WriteLine($"StackIndex {stackIndex}");
+                }
+                int numOfSlashes = NumOfSlashesInPath(charArray);
                 var args = new FileFoundArgs(file, numOfSlashes);
                 FileFound?.Invoke(this, args);
                 //if (args.CancelRequested)
                   //  break;
             }
         }
-        internal int numOfSlashesInPath(string directory,string dir)
+        internal void GetCharArrayBySlices(char[] charArray, Stack<int> stackPlacement)
+        {
+            int pop = 0;
+            string wasCharArray = charArray.ToString();
+            for(int i = pop; i<stackPlacement.Count; i++)
+            {
+                pop = stackPlacement.Pop();
+                if(pop != 0){
+                string folderOrFile = wasCharArray.Substring(i, pop);
+                i = pop;
+                }
+            }
+        }
+        internal void GetCharArrayBySlicesQueue(char[] charArray, Queue<int> slashPlacement)
+        {
+            int start = 0;
+            int capacity = slashPlacement.Count;
+            List<string> wordsBetweenSlashes = new List<string>();
+            for(int i = 0; i < capacity;i++)
+            {
+                string wasCharArray = new String(charArray);
+                int length = wasCharArray.Length;
+                int pop = slashPlacement.Dequeue();
+                if(pop != 0)
+                {
+                    System.Console.WriteLine(wasCharArray);
+                    System.Console.WriteLine(wasCharArray.Substring(start + 1, pop -start -1));
+                wordsBetweenSlashes.Add(wasCharArray.Substring(start + 1, pop - start - 1));
+                start = pop;
+                }
+                if(slashPlacement.Count==0)
+                {
+                    wordsBetweenSlashes.Add(wasCharArray.Substring(start + 1));
+                    System.Console.WriteLine(wasCharArray.Substring(start + 1));
+                }
+            }
+        }
+        internal char[] GetDirectoryCharArray(string directory, string dir)
         {
             var lastIndexOf = directory.LastIndexOf("/");
-            System.IO.DirectoryInfo directoryInfo = System.IO.Directory.GetParent(dir);
-            string directoryInfoString = directoryInfo.ToString();
-            string directoryInfoSubstring = directoryInfoString.Substring(lastIndexOf);
+            var directoryInfoSubstring = dir.Substring(45);
             var directoryInfoSubstringToCharArray = directoryInfoSubstring.ToCharArray();
+            return directoryInfoSubstringToCharArray;
+        }
+        internal int NumOfSlashesInPath(char[] directoryInfoSubstringToCharArray)
+        {
             int numOfSlashes = 0;
             //int totalNumOfSlashes = 0;
             for(int i = 0; i < directoryInfoSubstringToCharArray.Length; i++)
@@ -75,6 +134,32 @@ namespace ConsoleApplication
                 }
             }
             return numOfSlashes;
+        }
+        internal Stack<int> BuildDirectoryTree(char[] directoryInfoSubstringToCharArray)
+        {
+            for(int i = 0; i < directoryInfoSubstringToCharArray.Length; i++)
+            {
+                char current = directoryInfoSubstringToCharArray[i];
+                if(current.Equals('/'))
+                {
+                    if(current.Equals(null)){break;}
+                    SlashPlacementStack.Push(i);
+                }
+            }
+            return SlashPlacementStack;
+        }
+        internal Queue<int> BuildDirectoryTreeViaQueue(char[] directoryInfoSubstringToCharArray)
+        {
+            for(int i = 0; i < directoryInfoSubstringToCharArray.Length; i++)
+            {
+                char current = directoryInfoSubstringToCharArray[i];
+                if(current.Equals('/'))
+                {
+                    if(current.Equals(null)){break;}
+                    SlashPlacementQueue.Enqueue(i);
+                }
+            }
+            return SlashPlacementQueue;
         }
         internal void DirectoryInfo(string directory,string dir)
         {
